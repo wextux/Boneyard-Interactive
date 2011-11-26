c41c2697ebdf60af920f068abb305e87bf01e3a7
Will Fairclough <willbf6@gmail.com>
2011-11-26 16:08:13 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boneyard::Application.config.secret_token = '50cf22d0f5f9cea3ce9478dae8b48398b0bae5b5585ce392bb612f76427bbf638e73354105a4337fa61aa2416e45465a8493d643dee4b897703b71d1fc571eca'
